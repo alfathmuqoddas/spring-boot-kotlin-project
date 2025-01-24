aa7d9d33e1d193a04423a448d4b4a3dbba8592a7
Muhammad Alfath Muqoddas <821853@hq.bni.co.id>
2025-01-24 16:47:03 +0700

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,10 +23,12 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	runtimeOnly("com.h2database:h2")
 }
 
 kotlin {
--- src/main/kotlin/com/example/demo/model/Product.kt
+++ src/main/kotlin/com/example/demo/model/Product.kt
@@ -4,9 +4,16 @@ import javax.persistence.*
 
 @Entity
 data class Product(
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
+
+    @Column(nullable = false)
     val name: String,
+
+    @Column(nullable = false)
     val price: Double,
+
+    @Column(nullable = false)
     val quantity: Int
 )
